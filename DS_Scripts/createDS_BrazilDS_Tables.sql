60a88c8b372fb844c66f33d044a455d43036105d
soulfulArt <paulofariamelo@gmail.com>
2020-12-19 08:35:10 -0300

    mysql scrip for ds model ready

--- DS_Scripts/createDS_BrazilDS_Tables.sql
+++ DS_Scripts/createDS_BrazilDS_Tables.sql
@@ -3,4 +3,46 @@
 
 CREATE DATABASE BRAZILDS;
 
+CREATE TABLE Regiao (
 
+	Regiao_ID INT Primary Key NOT NULL,
+	Regiao_Nome varchar (12),
+	Regiao_Sigla varchar (2)
+
+);
+
+CREATE TABLE Estados (
+
+	Estado_ID INT Primary Key NOT NULL,
+	Estado_Nome varchar (22),
+	Regiao_ID INT,
+	FOREIGN KEY (Regiao_ID) REFERENCES Regiao (Regiao_ID)
+
+);
+
+CREATE TABLE Mesorregiao (
+
+	Mesorregiao_ID INT Primary Key NOT NULL,
+	Mesorregiao_Nome varchar (40),
+	Estado_ID INT,
+	FOREIGN KEY (Estado_ID) REFERENCES Estados (Estado_ID)
+
+);
+
+CREATE TABLE Microrregiao (
+
+	Microrregiao_ID INT Primary Key NOT NULL,
+	Microrregiao_Nome varchar (40),
+	Mesorregiao_ID INT,
+	FOREIGN KEY (Mesorregiao_ID) REFERENCES Mesorregiao (Mesorregiao_ID)
+
+);
+
+CREATE TABLE Municipios (
+
+	Municipios_ID INT Primary Key NOT NULL,
+	Municipios_Nome varchar (40),
+	Microrregiao_ID INT,
+	FOREIGN KEY (Microrregiao_ID) REFERENCES Microrregiao (Microrregiao_ID)
+
+);
